41343c15f07c53a566bac8546a6e6da6ec79b925
unknown <abuabdurehman0308@gmail.com>
2025-12-05 07:57:29 +0300

    Day 2 adding navigation website to github on friday 26/03/2018

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const lists = document.querySelectorAll(".li");
 const op = document.querySelector(".op");
+const glass = document.querySelector(".glass");
+
 
 lists.forEach((list) => {
     list.addEventListener("click", () => {
@@ -8,7 +10,18 @@ lists.forEach((list) => {
         })
         list.classList.add('active')
         let postionX = list.offsetLeft;
-        console.log(postionX)
         op.style.left = postionX + "px";
     });
+
+    list.addEventListener('mousemove', () => {
+        let postionX = list.offsetLeft;
+        let postionY = list.offsetTop;
+        glass.style.left = postionX + "px";
+        glass.style.top = postionY + "px";
+        glass.classList.add('h')
+    })
+
+    list.addEventListener('mouseleave', () => {
+        glass.classList.remove('h')
+    })
 });
